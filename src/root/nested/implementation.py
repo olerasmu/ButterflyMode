af8673fe014a1c6c86a914409a5db37d6ccee153
olerasmu <ole.gilberg@gmail.com>
2014-03-26 16:50:36 +0100

    small changes due to testing of reverse hg class

--- src/root/nested/implementation.py
+++ src/root/nested/implementation.py
@@ -4,13 +4,8 @@ Created on 18. mars 2014
 @author: olerasmu
 '''
 
-import os
-import binascii
-from _elementtree import Element
-import uu
-from base64 import encode
 import math
-from Crypto.Cipher import XOR, AES
+from Crypto.Cipher import AES
 
 
 #===============================================================================
@@ -36,20 +31,25 @@ class Butterfly(object):
         print (size)
         return size
     
-    #Slit the file into blocks of 64 bit (8*8 bytes)    
+    #Slit the file into blocks of 128 bit (16*8 bytes)    
     def blockifyFile(self):
         with open(self.filepath, 'r') as newfile:
             byte = newfile.read(16)
             while byte:
                 self.fileTab.append(byte)
-              # print byte
+                # print byte
                 byte = newfile.read(16)
-            print ("File has been divided into blocks")
+            #print ("File has been divided into blocks")
+    
+    def fileifyBlocks(self):
+        with open('bf_hourglass.txt', 'a') as hourglass_file:
+            for byte in self.butterflyTab:
+                hourglass_file.write(byte)
     
     #Initialize the butterfly function. Variable j is controlled from here
     def initiateButterfly(self, d, n):
         self.butterflyTab = [None]*n
-        print ("initiate")
+        #print ("initiate")
         for j in range(1, d+1):
             #print "this is j: ", j
             self.executeButterfly(j, n)
@@ -57,73 +57,74 @@ class Butterfly(object):
     count = 0
     #Execute the butterfly algorithm
     def executeButterfly(self, j, n):
-        print ("execute")
-        print ("this is j: ", j)    
+        #print ("execute")
+        #print ("this is j: ", j)    
         for k in range(0, int((n/math.pow(2, j))-1)+1):
-            print ("this is k: ", k)
-            for i in range(1, int(math.pow(2, j-1))+1):
-                indexOne = int(i+k*math.pow(2, j))-1
-                indexTwo = int(i+k*math.pow(2, j)+math.pow(2, j-1))-1
-                self.count += 1
-                self.w(self.fileTab[indexOne], self.fileTab[indexTwo], indexOne, indexTwo)
-                print ("this is i: ", i)
+            #print ("this is k: ", k)
+            if j == 1:
+                for i in range(1, int(math.pow(2, j-1))+1):
+                    indexOne = int(i+k*math.pow(2, j))-1
+                    indexTwo = int(i+k*math.pow(2, j)+math.pow(2, j-1))-1
+                    self.count += 1
+                    self.w(self.fileTab[indexOne], self.fileTab[indexTwo], indexOne, indexTwo)
+                    
+                    #print indexOne, " ", indexTwo
+            else:        
+                for i in range(1, int(math.pow(2, j-1))+1):
+                    indexOne = int(i+k*math.pow(2, j))-1
+                    indexTwo = int(i+k*math.pow(2, j)+math.pow(2, j-1))-1
+                    self.count += 1
+                    self.w(self.butterflyTab[indexOne], self.butterflyTab[indexTwo], indexOne, indexTwo)
+                    
+                    #print indexOne, " ", indexTwo
+                #print ("this is i: ", i)
+                #print ("this is index one: ", indexOne, " and this is index two: ", indexTwo)
                 #===============================================================
                 # temp = [int(i+k*math.pow(2, j))-1, int(i+k*math.pow(2, j)+math.pow(2, j-1))-1]
                 # print temp
                 #===============================================================
-        print ("count: ", self.count)
-    
+        #print ("count: ", self.count)
+        print (self.butterflyTab)
     
     #Some cryptographic operation
     def w(self, blockOne, blockTwo, indexOne, indexTwo):
-        print ("this is a crypto operation ")
+        #print ("this is a crypto operation ")
 
-        #=======================================================================
-        # cipher_machine = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-        #=======================================================================
+        cipher_machine = AES.new('This is a key123', AES.MODE_ECB)
         
-        print (len(blockOne))
-       
+        #print (len(blockOne))
+        
         #=======================================================================
+        # #Uncomment this to deactivate interleaving and encryption 
+        # print ("one ", indexOne, " two: ", indexTwo) 
+        # print ("Before: ", self.butterflyTab[indexOne], " ", self.butterflyTab[indexTwo]) 
         # self.butterflyTab[indexOne] = blockTwo
         # self.butterflyTab[indexTwo] = blockOne
         #=======================================================================
+         
+         
+        #print ("After: ", self.butterflyTab[indexOne], " ", self.butterflyTab[indexTwo])
         
+        #Comment this to deactivate interleaving and encryption
         interleaved  = "".join(str(i) for j in zip(blockOne,blockTwo) for i in j)
-          
+              
         new_block_one, new_block_two = interleaved[:int(len(interleaved)/2)], interleaved[int(len(interleaved)/2):]
+           
+        print (new_block_one)
+           
+        new_block_one = cipher_machine.encrypt(new_block_one)
+        new_block_two = cipher_machine.encrypt(new_block_two)
+          
+        print (new_block_one)
+          
         self.butterflyTab[indexOne] = new_block_two
         self.butterflyTab[indexTwo] = new_block_one
-      
-        #=======================================================================
-        # self.butterflyTab[indexOne] = cipher_machine.encrypt(blockTwo)
-        # self.butterflyTab[indexTwo] = cipher_machine.encrypt(blockOne)
-        #=======================================================================
-        
-        #=======================================================================
-        # temp_xor = blockOne^blockTwo
-        # print temp_xor
-        # self.butterflyTab.append(blockOne)    
-        # self.butterflyTab.append(blockTwo)
-        #=======================================================================
+           
+        temp = cipher_machine.decrypt(new_block_one)
+        print "This is decrypted: ", temp
         
         
         
-        #=======================================================================
-        # for a,b in zip(blockOne, blockTwo):
-        #     print a, " ", b
-        #     print ord(a), " ", ord(b)
-        #     print chr(59)
-        #     #print chr(ord(a)^ord(b))
-        #=======================================================================
-        
-        #=======================================================================
-        # temp = ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(blockOne,blockTwo))
-        #=======================================================================
-        
-        
-        #temp = chr(ord(blockOne)^ord(blockTwo))
-        #print temp
         
     #===========================================================================
     # def testOpen(self):
@@ -142,24 +143,31 @@ bf = Butterfly(filepath="butterflyfile.txt")
 
 bf.blockifyFile()
 n = int(bf.fileSize()/16)
-print (int(math.pow(2, 1-1)))
-print (n) 
+#print (int(math.pow(2, 1-1)))
+#print (n) 
 d = int(math.log(n, 2))
-bf.initiateButterfly(1, n)
-print ("This is d: ", d)
+bf.initiateButterfly(d, n)
+#print ("This is d: ", d)
  
-print (len(bf.butterflyTab))
-print (len(bf.fileTab))
+#===============================================================================
+# print (len(bf.butterflyTab))
+# print (len(bf.fileTab))
+#===============================================================================
 
 #Try to xor the blocks together 
 #temp = bf.w('a', 'b')
 #bf.w(temp, 'b')
 
+#Uncomment this to write the bf table to file.
+bf.fileifyBlocks()
+
 print (bf.fileTab)
 print (bf.butterflyTab)
- 
+print len(bf.butterflyTab)
+print (bf.count)
+
 #===============================================================================
-# obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-# print obj.decrypt('\xb0\x05:w\xe6"\xabp\xc4\x1b@d\xc8\xdf}\xa3')
+# for byte in bf.butterflyTab:
+#     print byte, " byte"
 #===============================================================================
- 
+
